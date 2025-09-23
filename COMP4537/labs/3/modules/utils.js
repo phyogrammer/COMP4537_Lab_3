b94480f379a36d78da1c3965d8829342dcc02fce
Phyo Thu Kha <phyotkha@outlook.com>
2025-09-23 16:07:24 -0700

    Add font-weight to greetings

--- COMP4537/labs/3/modules/utils.js
+++ COMP4537/labs/3/modules/utils.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+
 const getDate = function () {
     return Date();
 }
 
-export { getDate }
+const appendToFile = function (filename, content) {
+    fs.appendFile(filename, content + "\n", (err) => {
+        if (err) {
+            return err;
+        }
+    });
+    return (`The content "${content}" was appended to file ${filename}`);
+}
+
+const readFromFile = function (filename) {
+    fs.readFile(filename, "utf8", (err, data) => {
+        if (err) {
+            return err;
+        }
+        return data;
+    });
+}
+
+export { getDate, appendToFile, readFromFile }
--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,19 +1,56 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
 const greetings = require("./lang/en/en.js");
-const datatime = require("./modules/utils.js");
+const utils = require("./modules/utils.js");
 
-http.createServer(function(req, res){
+const FILENAME = "file.txt";
+
+http.createServer(function (req, res) {
     const query = url.parse(req.url, true);
 
-    const name = query.query["name"];
-    const presonalizedGreetings = `${greetings.greetings.replace("%1", name)} ${datatime.getDate()}`;
+    console.log(query);
 
-    res.writeHead(200, {
-        "Content-type": "text/html"
-    });
-    res.end(presonalizedGreetings);
+    if (query.pathname === "/writeFile/") {
+        const text = query.query["text"];
+
+        fs.appendFile(FILENAME, text + "\n", (err) => {
+            if (err) {
+                res.writeHead(200, {
+                    "Content-type": "text/html"
+                });
+                res.end(err);
+            }
+
+            res.writeHead(200, {
+                "Content-type": "text/html"
+            });
+            res.end(`The content "${text}" was appended to file ${FILENAME}`);
+        });
+    } else if (query.pathname === "/readFile/file.txt") {
+        fs.readFile(FILENAME, "utf8", (err, data) => {
+            if (err) {
+                res.writeHead(200, {
+                    "Content-type": "text/html"
+                });
+                res.end(err);
+            }
+
+            res.writeHead(200, {
+                "Content-type": "text/html"
+            });
+            res.end(data);
+        });
+    } else {
+        const name = query.query["name"];
+        const presonalizedGreetings = `<span style="color: blue; font-weight: bold">${greetings.greetings.replace("%1", name)} ${utils.getDate()}</span>`;
+
+        res.writeHead(200, {
+            "Content-type": "text/html"
+        });
+        res.end(presonalizedGreetings);
+    }
 }).listen(8080);
 
 console.log(`Server is running and listening...`);
